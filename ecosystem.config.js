5f65417016c880253dbd29f8bde951c3c855fd95
Rachel Perez <reperez@me.com>
2020-01-02 20:07:22 -0500

    fixed config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
         host: 'ec2-3-19-26-24.us-east-2.compute.amazonaws.com',
         key: '~/.ssh/a.pem',
         ref: 'origin/master',
-        repo: 'git@github.com:reperez/server-a.git',
+        repo: 'git@github.com:reperez120/server-a.git',
         path: '/home/ubuntu/server-a',
         'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
       }
