a02d2a52f17d6293ba4bd3c4f78b220291171c31
Rachel Perez <reperez@me.com>
2020-01-02 20:20:30 -0500

    edited config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,18 @@
 module.exports = {
-    apps: [{
-      name: 'server-a',
-      script: './index.js'
-    }],
-    deploy: {
-      production: {
-        user: 'ubuntu',
-        host: 'ec2-3-19-26-24.us-east-2.compute.amazonaws.com',
-        key: '~/.ssh/a.pem',
-        ref: 'origin/master',
-        repo: 'git@github.com:reperez120/server-a.git',
-        path: '/home/ubuntu/server-a',
-        'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
-      }
+  apps : [{
+    name: 'server-a',
+    script: './index.js'
+  }],
+
+  deploy : {
+    production : {
+      user : 'ubuntu',
+      host : 'ec2-3-19-26-24.us-east-2.compute.amazonaws.com/',
+      key: '~./ssh/a.pem',
+      ref  : 'origin/master',
+      repo : 'git@github.com:reperez120/server-a.git',
+      path : 'home/ubuntu/server-a',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
     }
   }
+};
